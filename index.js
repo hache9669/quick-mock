cdd20043b6c6734c61b00e5446eaf5c8a69b0390
hache9669 <hache9669@gmail.com>
2024-08-13 23:27:56 +0900

    add express

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'hello' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
